d530613b3e186d75d8af6f90026dfe21f398c9ff
Manohar Castelino <manohar.r.castelino@intel.com>
2017-07-21 10:37:38 -0700

    elf_dump: Switched to using template functions
    Signed-off-by: Manohar Castelino <manohar.r.castelino@intel.com>

--- elf_dump/elf_dump.go
+++ elf_dump/elf_dump.go
@@ -15,36 +15,72 @@ import (
 var cfg *tfortools.Config
 var code string //The format string
 
-var fileHeader elf.FileHeader
-var progHeaders []elf.ProgHeader
-var sectionHeaders []elf.SectionHeader
-
 func init() {
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s \n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Usage: %s [-f <template>] <filename> \n", os.Args[0])
 		flag.PrintDefaults()
 		fmt.Fprintf(os.Stderr, "The template passed to the -%s option for FileHeader operates on a\n\n", "f")
-		fmt.Fprintln(os.Stderr, tfortools.GenerateUsageUndecorated(fileHeader))
+		fmt.Fprintln(os.Stderr, tfortools.GenerateUsageUndecorated(elf.FileHeader{}))
 		fmt.Fprintf(os.Stderr, "The template passed to the -%s option for ProgramHeaders operates on a\n\n", "f")
-		fmt.Fprintln(os.Stderr, tfortools.GenerateUsageUndecorated(progHeaders))
+		fmt.Fprintln(os.Stderr, tfortools.GenerateUsageUndecorated([]elf.ProgHeader{}))
 		fmt.Fprintf(os.Stderr, "The template passed to the -%s option for Sections operates on a\n\n", "f")
-		fmt.Fprintln(os.Stderr, tfortools.GenerateUsageUndecorated(sectionHeaders))
+		fmt.Fprintln(os.Stderr, tfortools.GenerateUsageUndecorated([]elf.SectionHeader{}))
 		fmt.Fprintln(os.Stderr, tfortools.TemplateFunctionHelp(cfg))
 	}
+
 	flag.StringVar(&code, "f", "", "string containing the template code to execute")
+
 	cfg = tfortools.NewConfig(tfortools.OptAllFns)
+
+	if err := cfg.AddCustomFn(getSections, "getSections", getSectionsHelp); err != nil {
+		panic(err)
+	}
+
+	if err := cfg.AddCustomFn(getPrograms, "getPrograms", getProgramsHelp); err != nil {
+		panic(err)
+	}
+}
+
+const getSectionsHelp = "- getSections extracts all the sections from the elf \n"
+
+func getSections(f *elf.File) []elf.SectionHeader {
+	var sectionHeaders []elf.SectionHeader
+
+	for _, s := range f.Sections {
+		sectionHeaders = append(sectionHeaders, s.SectionHeader)
+	}
+	return sectionHeaders
+}
+
+const getProgramsHelp = "- getPrograms extracts all the sections from the elf \n"
+
+func getPrograms(f *elf.File) []elf.ProgHeader {
+	var progHeaders []elf.ProgHeader
+
+	for _, p := range f.Progs {
+		progHeaders = append(progHeaders, p.ProgHeader)
+	}
+	return progHeaders
 }
 
 func main() {
-	filename := flag.String("filename", "", "file to dump")
-	component := flag.String("component", "",
-		"file component to dump (file|section|program), if unspecified dumps all possible headers")
-
 	flag.Parse()
 
-	contents, err := ioutil.ReadFile(*filename)
+	if len(flag.Args()) < 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	filename := flag.Args()[0]
+	if filename == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	contents, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Fatal(err)
+		os.Exit(1)
 	}
 
 	r := bytes.NewReader(contents)
@@ -52,47 +88,36 @@ func main() {
 	f, err := elf.NewFile(r)
 	if err != nil {
 		log.Fatal(err)
+		os.Exit(1)
 	}
 
-	fileHeader = f.FileHeader
-
-	for _, p := range f.Progs {
-		progHeaders = append(progHeaders, p.ProgHeader)
-	}
-
-	for _, s := range f.Sections {
-		sectionHeaders = append(sectionHeaders, s.SectionHeader)
-	}
-
-	if *component == "" {
-		if code == "" {
-			fmt.Println("\nFile Header:")
-			tfortools.OutputToTemplate(os.Stdout, "File", "{{println .}}", fileHeader, cfg)
-			fmt.Printf("\nProgram Headers:")
-			tfortools.OutputToTemplate(os.Stdout, "Program headers", "{{table .}}", progHeaders, cfg)
-			fmt.Println("\nSection Headers:")
-			tfortools.OutputToTemplate(os.Stdout, "Sections", "{{table .}}", sectionHeaders, cfg)
-		} else {
-			fmt.Println("\nFile Header:")
-			tfortools.OutputToTemplate(os.Stdout, "File", code, fileHeader, cfg)
-			fmt.Printf("\nProgram Headers:")
-			tfortools.OutputToTemplate(os.Stdout, "Program headers", code, progHeaders, cfg)
-			fmt.Println("\nSection Headers:")
-			tfortools.OutputToTemplate(os.Stdout, "Sections", code, sectionHeaders, cfg)
+	if code == "" {
+		fmt.Printf("\nFile Header:\n")
+		err = tfortools.OutputToTemplate(os.Stdout, "File", "{{println .}}", f.FileHeader, cfg)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
 		}
-		return
+
+		fmt.Printf("\nProgram Headers:\n")
+		tfortools.OutputToTemplate(os.Stdout, "Program headers", "{{ table (getPrograms .) }}", f, cfg)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+		fmt.Printf("\nSection Headers:\n")
+		tfortools.OutputToTemplate(os.Stdout, "Sections", "{{ table (getSections .) }}", f, cfg)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		os.Exit(0)
 	}
 
-	switch *component {
-	case "program":
-		err = tfortools.OutputToTemplate(os.Stdout, "Program headers", code, progHeaders, cfg)
-	case "file":
-		err = tfortools.OutputToTemplate(os.Stdout, "File header", code, fileHeader, cfg)
-	case "section":
-		err = tfortools.OutputToTemplate(os.Stdout, "Sections", code, sectionHeaders, cfg)
-	}
-
+	err = tfortools.OutputToTemplate(os.Stdout, "f", code, f, cfg)
 	if err != nil {
-		fmt.Errorf("Unable to execute template : %v", err)
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
 	}
 }
